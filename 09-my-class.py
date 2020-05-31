62794edc2328582572841e95a1f7ae875195536b
h-github <hamid.am7777@gmail.com>
2020-05-31 00:46:08 -0700

    Class and Methods

---
+++ 09-my-class.py
@@ -0,0 +1,33 @@
+class Car:
+    def __init__(self, **kwargs):
+        self._make = kwargs['make'] if 'make' in kwargs else 'BMW'
+        self._model = kwargs['model'] if 'model' in kwargs else 'MX5'
+        self._year = kwargs['year'] if 'year' in kwargs else '2020'
+
+    def make(self, mk=None):
+        if mk:
+            self._make = mk
+        return self._make
+
+    def model(self, md=None):
+        if md:
+            self._model = md
+        return self._model
+
+    def year(self, y=None):
+        if y:
+            self._year = y
+        return self._year
+
+    def __str__(self):
+        return f'{self._year} {self._make} {self._model}'
+
+
+def main():
+    c1 = Car(model='Civic', make='Honda', year=2020)
+    print(c1)
+    print(Car())
+
+
+if __name__ == "__main__":
+    main()

03bd21aa6f9bc01d442c6ae38398327ddb7c7962
h-github <hmohammadi@hotmail.ca>
2020-05-30 23:18:55 -0700

    Set

---
+++ 08-sets.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+
+def main():
+    a = set("We're gonna need a bigger boat.")
+    b = set("I'm sorry, Dave. I'm afraid I can't do that.")
+    print_set(a)
+    print_set(b)
+    print_set(sorted(a))
+    print_set(sorted(b))
+
+    # in a not in b
+    print_set(a - b)
+
+    # in a or in b
+    print_set(a | b)
+
+    # in a or in b, but not both. exclusive or
+    print_set(a ^ b)
+
+    # in a and in b, both.
+    print_set(a & b)
+
+
+def print_set(o):
+    print('{', end='')
+    for x in o:
+        print(x, end='')
+    print('}')
+
+
+if __name__ == '__main__':
+    main()

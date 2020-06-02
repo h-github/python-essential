b9695e8710da4ea28fc1a0843912739ca0bc5b70
h-github <hmohammadi@hotmail.ca>
2020-06-01 20:51:12 -0700

    Final Class

---
+++ 09-hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+
+def main():
+    x = [1, 2, 3, 4, 5, 73, 2, 4, 67, 8]
+    print(x[::-2])
+
+
+if __name__ == '__main__':
+    main()
